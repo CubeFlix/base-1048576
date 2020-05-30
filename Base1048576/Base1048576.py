a74e153df2a3c82d2f67fb12b5038a15037f632d
CubeFlix <55668748+CubeFlix@users.noreply.github.com>
2020-05-30 07:52:16 -0500

    first commit. not working with decimals yet.
    first commit. not working with decimals yet.

---
+++ Base1048576/Base1048576.py
@@ -0,0 +1,97 @@
+# Base 1,048,576 Converter
+# By Kevin Chen
+
+# Note: When converting decimals, a character of unicode '\U00100004' will be added, to represent a decimal point, as it is out of the range of characters 1-1048576.
+
+#todo: has no support for decimals
+
+import math
+
+def splitnum(num): 
+    return [char for char in num]
+
+def convert(s): 
+  
+    new = "" 
+  
+    for x in s: 
+        new += x  
+  
+    return new 
+
+
+def ToBase1048576(num):
+    
+    if num < 0:
+        sign = -1
+    elif num == 0:
+        return 0
+    else:
+        sign = 1
+
+    num *= sign
+
+    digits = []
+    decimalDigits = []
+
+
+    splitnumbers = [int(str(num).split(".")[0]), int(str(num).split(".")[1])]
+    splitnumbers.reverse()
+    print(splitnumbers)
+
+    while splitnumbers[1]:
+        n = splitnumbers[1] % 1048576
+        digits.append(chr(int(n)))
+        
+        splitnumbers[1] = int(splitnumbers[1]/1048576)
+
+    if sign < 0:
+        digits.append('-')
+
+    digits.reverse()
+
+    decimalNums = splitnumbers[0]
+    while decimalNums:
+        n = decimalNums % 1048576
+        decimalDigits.append(chr(int(n)))
+        
+        decimalNums = int(decimalNums/1048576)
+
+    
+    
+    if splitnumbers[0] == 0:
+        return convert(digits)
+    else:
+        return convert(digits + [chr(1048580)] + decimalDigits)
+
+
+def FromBase1048576(unicodestring, posiitivity):
+    chars = splitnum(unicodestring)
+
+    digits = []
+    
+    if not posiitivity:
+        
+        for char in range(1, len(chars)):
+            digits.append(ord(chars[char]))
+
+        digits.reverse()
+
+        for num in digits:
+            final += (num * pow(1048576, (digits.index(num-1))))
+        return -final
+
+                
+    else:
+        for char in range(0, len(chars)):
+            digits.append(ord(chars[char]))
+
+            final = 0
+            
+        digits.reverse()
+
+        for num in digits:
+            final += (num * pow(1048576, (digits.index(num))))
+        return final
+
+	
