23fe1547df5695440a8ea5921a74fb5599bf4cba
CubeFlix <55668748+CubeFlix@users.noreply.github.com>
2020-06-05 17:02:24 -0500

    making decimals a thing
    a

--- Base1048576/Base1048576.py
+++ Base1048576/Base1048576.py
@@ -2,10 +2,13 @@
 # By Kevin Chen
 
 # Note: When converting decimals, a character of unicode '\U00100004' will be added, to represent a decimal point, as it is out of the range of characters 1-1048576.
+# Also, negitive signs will be represented by '\U00100005'.
 
-#todo: has no support for decimals
+#todo: has no support for decimals or addng and subtracting, etc.
 
 import math
+
+MAX_DECIMAL_PLACES = 10
 
 def splitnum(num): 
     return [char for char in num]
@@ -25,7 +28,7 @@ def ToBase1048576(num):
     if num < 0:
         sign = -1
     elif num == 0:
-        return 0
+        return '\x00'
     else:
         sign = 1
 
@@ -33,79 +36,96 @@ def ToBase1048576(num):
 
     digits = []
     decimalDigits = []
-
-
-    splitnumbers = [int(str(num).split(".")[0]), int(str(num).split(".")[1])]
+    hasDecimals = True
+    try:
+        splitnumbers = [int(str(num).split(".")[0]), float("0."+str(num).split(".")[1])]
+    except:
+        splitnumbers = [int(str(num).split(".")[0]), 0]
+        hasDecimals = False
     splitnumbers.reverse()
-    print(splitnumbers)
 
     while splitnumbers[1]:
         n = splitnumbers[1] % 1048576
         digits.append(chr(int(n)))
-        
         splitnumbers[1] = int(splitnumbers[1]/1048576)
 
     if sign < 0:
-        digits.append('-')
+        digits.append('\U00100005')
 
     digits.reverse()
-
-    decimalNums = splitnumbers[0]
-    while decimalNums:
-        n = decimalNums % 1048576
-        decimalDigits.append(chr(int(n)))
-        
-        decimalNums = int(decimalNums/1048576)
-
+    
+    if hasDecimals:
+        lastNumber = splitnumbers[0]
+        while lastNumber > 0 and len(decimalDigits) < MAX_DECIMAL_PLACES:
+            decimalDigits.append(chr(int(str(lastNumber * 1048576).split('.')[0])))
+            lastNumber = float("0."+str(lastNumber * 1048576).split('.')[1])
     
     
-    if splitnumbers[0] == 0:
+    if not hasDecimals:
         return convert(digits)
     else:
         return convert(digits + [chr(1048580)] + decimalDigits)
 
 
-def FromBase1048576(unicodestring, posiitivity):
-    chars = unicodestring.split('.')
+def FromBase1048576(unicodestring):
+    chars = unicodestring.split('\U00100004')
     upperchars = splitnum(chars[0])
-    lowerchars = splitnum(chars[1])
+    hasDecimals = True
+    try:
+        lowerchars = splitnum(chars[1])
+    except:
+        hasDecimals = False
+        
 
     digits = []
     lowerdigits = []
 
     
-    if not posiitivity:
+    if upperchars[0] == '\U00100005':
         for upperchar in range(1, len(upperchars)):
             digits.append(ord(upperchars[upperchar]))
 
         digits.reverse()
 
         for num in digits:
-            final += (num * pow(1048576, (digits.index(num-1))))
-
-        for lowerchar in range(1, len(lowerchars)):
-            lowerdigits.append(ord(lowerchars[lowerchar]))
-
-        lowerdigits.reverse()
-
-        for lowernum in lowerdigits:
-            final += (num * pow(1048576, -(digits.index(num-1))))
             
-        return -final
+            final += (num * pow(1048576, (digits.index(num-1))))
+        if hasDecimals:
+            
+            #lowerdigits.reverse()
 
-    
+            for lowerchar in range(1, len(lowerchars)):
+                lowerdigits.append(ord(lowerchars[lowerchar]))
+
+
+            for lowernum in lowerdigits:
+                final += (num * pow(1048576, -(digits.index(num-1))))
+                
+        return -final
 
                 
     else:
-        for char in range(0, len(chars)):
-            digits.append(ord(chars[char]))
-
-            final = 0
+        for upperchar in range(0, len(upperchars)):
+            digits.append(ord(upperchars[upperchar]))
+        final = 0
+        lowerfinal = 0
             
         digits.reverse()
 
         for num in digits:
             final += (num * pow(1048576, (digits.index(num))))
-        return final
+
+        if hasDecimals:
+            #lowerdigits.reverse()
+            
+            for lowerchar in range(0, len(lowerchars)):
+                lowerdigits.append(ord(lowerchars[lowerchar]))
+
+            for lowernum in lowerdigits:
+                lowerfinal += 1048576*lowernum
+            lowerfinal = lowerfinal/pow(10, len(str(lowerfinal)))
+            print(lowerfinal)
+            
+            return final + lowerfinal
 
 	
